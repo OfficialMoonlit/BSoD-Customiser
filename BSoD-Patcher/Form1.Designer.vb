390ba8fba795b41fd4cefd38b6e4b244cbf08ce4
Moonlit <mltgit@live.co.uk>
2012-12-02 11:40:38 +0000

    Version 1.2.2

---
+++ BSoD-Patcher/Form1.Designer.vb
@@ -0,0 +1,333 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtBlock1 = New System.Windows.Forms.TextBox()
+        Me.lblBlock1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.lblLength = New System.Windows.Forms.Label()
+        Me.lblBlock2 = New System.Windows.Forms.Label()
+        Me.txtBlock2 = New System.Windows.Forms.TextBox()
+        Me.lblBlock3 = New System.Windows.Forms.Label()
+        Me.txtBlock3 = New System.Windows.Forms.TextBox()
+        Me.lblBlock5 = New System.Windows.Forms.Label()
+        Me.txtBlock5 = New System.Windows.Forms.TextBox()
+        Me.lblBlock7 = New System.Windows.Forms.Label()
+        Me.txtBlock7 = New System.Windows.Forms.TextBox()
+        Me.lblBlock9 = New System.Windows.Forms.Label()
+        Me.txtBlock9 = New System.Windows.Forms.TextBox()
+        Me.cmdPatch = New System.Windows.Forms.Button()
+        Me.txtSource = New System.Windows.Forms.TextBox()
+        Me.txtDest = New System.Windows.Forms.TextBox()
+        Me.cmdSourceBrowse = New System.Windows.Forms.Button()
+        Me.cmdPatchedBrowse = New System.Windows.Forms.Button()
+        Me.chkStop = New System.Windows.Forms.CheckBox()
+        Me.cmdAbout = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtBlock1
+        '
+        Me.txtBlock1.BackColor = System.Drawing.Color.Navy
+        Me.txtBlock1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBlock1.ForeColor = System.Drawing.Color.White
+        Me.txtBlock1.Location = New System.Drawing.Point(12, 26)
+        Me.txtBlock1.Multiline = True
+        Me.txtBlock1.Name = "txtBlock1"
+        Me.txtBlock1.Size = New System.Drawing.Size(617, 39)
+        Me.txtBlock1.TabIndex = 0
+        '
+        'lblBlock1
+        '
+        Me.lblBlock1.AutoSize = True
+        Me.lblBlock1.BackColor = System.Drawing.Color.Transparent
+        Me.lblBlock1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBlock1.ForeColor = System.Drawing.Color.White
+        Me.lblBlock1.Location = New System.Drawing.Point(12, 10)
+        Me.lblBlock1.Name = "lblBlock1"
+        Me.lblBlock1.Size = New System.Drawing.Size(39, 15)
+        Me.lblBlock1.TabIndex = 1
+        Me.lblBlock1.Text = "(0/96)"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(12, 489)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(476, 21)
+        Me.TextBox2.TabIndex = 2
+        '
+        'lblLength
+        '
+        Me.lblLength.AutoSize = True
+        Me.lblLength.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLength.Location = New System.Drawing.Point(494, 492)
+        Me.lblLength.Name = "lblLength"
+        Me.lblLength.Size = New System.Drawing.Size(14, 15)
+        Me.lblLength.TabIndex = 3
+        Me.lblLength.Text = "0"
+        '
+        'lblBlock2
+        '
+        Me.lblBlock2.AutoSize = True
+        Me.lblBlock2.BackColor = System.Drawing.Color.Transparent
+        Me.lblBlock2.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBlock2.ForeColor = System.Drawing.Color.White
+        Me.lblBlock2.Location = New System.Drawing.Point(12, 68)
+        Me.lblBlock2.Name = "lblBlock2"
+        Me.lblBlock2.Size = New System.Drawing.Size(46, 15)
+        Me.lblBlock2.TabIndex = 5
+        Me.lblBlock2.Text = "(0/139)"
+        '
+        'txtBlock2
+        '
+        Me.txtBlock2.BackColor = System.Drawing.Color.Navy
+        Me.txtBlock2.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBlock2.ForeColor = System.Drawing.Color.White
+        Me.txtBlock2.Location = New System.Drawing.Point(12, 84)
+        Me.txtBlock2.Multiline = True
+        Me.txtBlock2.Name = "txtBlock2"
+        Me.txtBlock2.Size = New System.Drawing.Size(617, 40)
+        Me.txtBlock2.TabIndex = 4
+        '
+        'lblBlock3
+        '
+        Me.lblBlock3.AutoSize = True
+        Me.lblBlock3.BackColor = System.Drawing.Color.Transparent
+        Me.lblBlock3.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBlock3.ForeColor = System.Drawing.Color.White
+        Me.lblBlock3.Location = New System.Drawing.Point(12, 127)
+        Me.lblBlock3.Name = "lblBlock3"
+        Me.lblBlock3.Size = New System.Drawing.Size(46, 15)
+        Me.lblBlock3.TabIndex = 7
+        Me.lblBlock3.Text = "(0/273)"
+        '
+        'txtBlock3
+        '
+        Me.txtBlock3.BackColor = System.Drawing.Color.Navy
+        Me.txtBlock3.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBlock3.ForeColor = System.Drawing.Color.White
+        Me.txtBlock3.Location = New System.Drawing.Point(12, 143)
+        Me.txtBlock3.Multiline = True
+        Me.txtBlock3.Name = "txtBlock3"
+        Me.txtBlock3.Size = New System.Drawing.Size(617, 120)
+        Me.txtBlock3.TabIndex = 6
+        '
+        'lblBlock5
+        '
+        Me.lblBlock5.AutoSize = True
+        Me.lblBlock5.BackColor = System.Drawing.Color.Transparent
+        Me.lblBlock5.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBlock5.ForeColor = System.Drawing.Color.White
+        Me.lblBlock5.Location = New System.Drawing.Point(12, 266)
+        Me.lblBlock5.Name = "lblBlock5"
+        Me.lblBlock5.Size = New System.Drawing.Size(39, 15)
+        Me.lblBlock5.TabIndex = 11
+        Me.lblBlock5.Text = "(0/23)"
+        '
+        'txtBlock5
+        '
+        Me.txtBlock5.BackColor = System.Drawing.Color.Navy
+        Me.txtBlock5.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBlock5.ForeColor = System.Drawing.Color.White
+        Me.txtBlock5.Location = New System.Drawing.Point(12, 282)
+        Me.txtBlock5.Name = "txtBlock5"
+        Me.txtBlock5.Size = New System.Drawing.Size(199, 21)
+        Me.txtBlock5.TabIndex = 10
+        '
+        'lblBlock7
+        '
+        Me.lblBlock7.AutoSize = True
+        Me.lblBlock7.BackColor = System.Drawing.Color.Transparent
+        Me.lblBlock7.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBlock7.ForeColor = System.Drawing.Color.White
+        Me.lblBlock7.Location = New System.Drawing.Point(217, 266)
+        Me.lblBlock7.Name = "lblBlock7"
+        Me.lblBlock7.Size = New System.Drawing.Size(39, 15)
+        Me.lblBlock7.TabIndex = 15
+        Me.lblBlock7.Text = "(0/34)"
+        '
+        'txtBlock7
+        '
+        Me.txtBlock7.BackColor = System.Drawing.Color.Navy
+        Me.txtBlock7.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBlock7.ForeColor = System.Drawing.Color.White
+        Me.txtBlock7.Location = New System.Drawing.Point(217, 282)
+        Me.txtBlock7.Name = "txtBlock7"
+        Me.txtBlock7.Size = New System.Drawing.Size(319, 21)
+        Me.txtBlock7.TabIndex = 14
+        '
+        'lblBlock9
+        '
+        Me.lblBlock9.AutoSize = True
+        Me.lblBlock9.BackColor = System.Drawing.Color.Transparent
+        Me.lblBlock9.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBlock9.ForeColor = System.Drawing.Color.White
+        Me.lblBlock9.Location = New System.Drawing.Point(12, 306)
+        Me.lblBlock9.Name = "lblBlock9"
+        Me.lblBlock9.Size = New System.Drawing.Size(46, 15)
+        Me.lblBlock9.TabIndex = 19
+        Me.lblBlock9.Text = "(0/126)"
+        '
+        'txtBlock9
+        '
+        Me.txtBlock9.BackColor = System.Drawing.Color.Navy
+        Me.txtBlock9.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBlock9.ForeColor = System.Drawing.Color.White
+        Me.txtBlock9.Location = New System.Drawing.Point(12, 322)
+        Me.txtBlock9.Multiline = True
+        Me.txtBlock9.Name = "txtBlock9"
+        Me.txtBlock9.Size = New System.Drawing.Size(617, 38)
+        Me.txtBlock9.TabIndex = 18
+        '
+        'cmdPatch
+        '
+        Me.cmdPatch.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.cmdPatch.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdPatch.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.cmdPatch.Location = New System.Drawing.Point(12, 377)
+        Me.cmdPatch.Name = "cmdPatch"
+        Me.cmdPatch.Size = New System.Drawing.Size(179, 51)
+        Me.cmdPatch.TabIndex = 20
+        Me.cmdPatch.Text = "Patch Now"
+        Me.cmdPatch.UseVisualStyleBackColor = False
+        '
+        'txtSource
+        '
+        Me.txtSource.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSource.Location = New System.Drawing.Point(197, 377)
+        Me.txtSource.Name = "txtSource"
+        Me.txtSource.Size = New System.Drawing.Size(286, 21)
+        Me.txtSource.TabIndex = 21
+        '
+        'txtDest
+        '
+        Me.txtDest.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDest.Location = New System.Drawing.Point(197, 407)
+        Me.txtDest.Name = "txtDest"
+        Me.txtDest.Size = New System.Drawing.Size(286, 21)
+        Me.txtDest.TabIndex = 22
+        '
+        'cmdSourceBrowse
+        '
+        Me.cmdSourceBrowse.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.cmdSourceBrowse.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdSourceBrowse.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.cmdSourceBrowse.Location = New System.Drawing.Point(489, 377)
+        Me.cmdSourceBrowse.Name = "cmdSourceBrowse"
+        Me.cmdSourceBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSourceBrowse.TabIndex = 24
+        Me.cmdSourceBrowse.Text = "Source File"
+        Me.cmdSourceBrowse.UseVisualStyleBackColor = False
+        '
+        'cmdPatchedBrowse
+        '
+        Me.cmdPatchedBrowse.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.cmdPatchedBrowse.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdPatchedBrowse.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.cmdPatchedBrowse.Location = New System.Drawing.Point(489, 405)
+        Me.cmdPatchedBrowse.Name = "cmdPatchedBrowse"
+        Me.cmdPatchedBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.cmdPatchedBrowse.TabIndex = 25
+        Me.cmdPatchedBrowse.Text = "Patched File"
+        Me.cmdPatchedBrowse.UseVisualStyleBackColor = False
+        '
+        'chkStop
+        '
+        Me.chkStop.AutoSize = True
+        Me.chkStop.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkStop.ForeColor = System.Drawing.Color.White
+        Me.chkStop.Location = New System.Drawing.Point(542, 284)
+        Me.chkStop.Name = "chkStop"
+        Me.chkStop.Size = New System.Drawing.Size(90, 19)
+        Me.chkStop.TabIndex = 26
+        Me.chkStop.Text = "STOP Code"
+        Me.chkStop.UseVisualStyleBackColor = True
+        '
+        'cmdAbout
+        '
+        Me.cmdAbout.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.cmdAbout.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdAbout.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.cmdAbout.Location = New System.Drawing.Point(570, 377)
+        Me.cmdAbout.Name = "cmdAbout"
+        Me.cmdAbout.Size = New System.Drawing.Size(62, 51)
+        Me.cmdAbout.TabIndex = 27
+        Me.cmdAbout.Text = "About..."
+        Me.cmdAbout.UseVisualStyleBackColor = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Navy
+        Me.ClientSize = New System.Drawing.Size(642, 439)
+        Me.Controls.Add(Me.cmdAbout)
+        Me.Controls.Add(Me.chkStop)
+        Me.Controls.Add(Me.cmdPatchedBrowse)
+        Me.Controls.Add(Me.cmdSourceBrowse)
+        Me.Controls.Add(Me.txtDest)
+        Me.Controls.Add(Me.txtSource)
+        Me.Controls.Add(Me.cmdPatch)
+        Me.Controls.Add(Me.lblBlock9)
+        Me.Controls.Add(Me.txtBlock9)
+        Me.Controls.Add(Me.lblBlock7)
+        Me.Controls.Add(Me.txtBlock7)
+        Me.Controls.Add(Me.lblBlock5)
+        Me.Controls.Add(Me.txtBlock5)
+        Me.Controls.Add(Me.lblBlock3)
+        Me.Controls.Add(Me.txtBlock3)
+        Me.Controls.Add(Me.lblBlock2)
+        Me.Controls.Add(Me.txtBlock2)
+        Me.Controls.Add(Me.lblLength)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.lblBlock1)
+        Me.Controls.Add(Me.txtBlock1)
+        Me.Name = "frmMain"
+        Me.Text = "BSoD-Patcher 1.2.2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtBlock1 As System.Windows.Forms.TextBox
+    Friend WithEvents lblBlock1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents lblLength As System.Windows.Forms.Label
+    Friend WithEvents lblBlock2 As System.Windows.Forms.Label
+    Friend WithEvents txtBlock2 As System.Windows.Forms.TextBox
+    Friend WithEvents lblBlock3 As System.Windows.Forms.Label
+    Friend WithEvents txtBlock3 As System.Windows.Forms.TextBox
+    Friend WithEvents lblBlock5 As System.Windows.Forms.Label
+    Friend WithEvents txtBlock5 As System.Windows.Forms.TextBox
+    Friend WithEvents lblBlock7 As System.Windows.Forms.Label
+    Friend WithEvents txtBlock7 As System.Windows.Forms.TextBox
+    Friend WithEvents lblBlock9 As System.Windows.Forms.Label
+    Friend WithEvents txtBlock9 As System.Windows.Forms.TextBox
+    Friend WithEvents cmdPatch As System.Windows.Forms.Button
+    Friend WithEvents txtSource As System.Windows.Forms.TextBox
+    Friend WithEvents txtDest As System.Windows.Forms.TextBox
+    Friend WithEvents cmdSourceBrowse As System.Windows.Forms.Button
+    Friend WithEvents cmdPatchedBrowse As System.Windows.Forms.Button
+    Friend WithEvents chkStop As System.Windows.Forms.CheckBox
+    Friend WithEvents cmdAbout As System.Windows.Forms.Button
+
+End Class
---
+++ BSoD-Patcher/Form1.vb
@@ -0,0 +1,148 @@
+﻿Imports System.IO
+Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class frmMain
+    Dim strPad As String
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+        lblLength.Text = Len(TextBox2.Text)
+    End Sub
+
+    Private Sub txtBlock1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtBlock1.TextChanged
+        'strPad = Strings.Left(strPad.PadRight(94), 94)
+        'txtBlock1.Text = strPad
+        strPad = txtBlock1.Text
+        lblBlock1.Text = "(" & Len(Strings.Trim(strPad)) & "/96)"
+    End Sub
+
+    Private Sub txtBlock2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtBlock2.TextChanged
+        strPad = txtBlock2.Text
+        lblBlock2.Text = "(" & Len(Strings.Trim(strPad)) & "/139)"
+    End Sub
+
+    Private Sub txtBlock3_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtBlock3.TextChanged
+        strPad = txtBlock3.Text
+        lblBlock3.Text = "(" & Len(Strings.Trim(strPad)) & "/491)"
+    End Sub
+
+    Private Sub txtBlock5_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtBlock5.TextChanged
+        strPad = txtBlock5.Text
+        lblBlock5.Text = "(" & Len(Strings.Trim(strPad)) & "/23)"
+    End Sub
+
+    Private Sub txtBlock7_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtBlock7.TextChanged
+        strPad = txtBlock7.Text
+        lblBlock7.Text = "(" & Len(Strings.Trim(strPad)) & "/34)"
+    End Sub
+
+    Private Sub txtBlock9_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtBlock9.TextChanged
+        strPad = txtBlock9.Text
+        lblBlock9.Text = "(" & Len(Strings.Trim(strPad)) & "/126)"
+    End Sub
+
+    Private Sub cmdPatch_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdPatch.Click
+        Dim x As New MsgBoxResult
+
+        If txtSource.Text = txtDest.Text Then
+            MsgBox("Source and destination file names are identical." & vbCrLf & "Please choose another destination file name or location.", vbOKOnly + MsgBoxStyle.Exclamation, "BSoD-Patcher")
+            Exit Sub
+        ElseIf txtSource.Text = "" Then
+            MsgBox("Invalid source file, please choose another file name or location.", vbOKOnly + MsgBoxStyle.Exclamation, "BSoD-Patcher")
+            Exit Sub
+        ElseIf txtDest.Text = "" Then
+            MsgBox("Invalid destination file, please choose another file name or location.", vbOKOnly + MsgBoxStyle.Exclamation, "BSoD-Patcher")
+            Exit Sub
+        End If
+
+        Try
+            Dim md5 As MD5CryptoServiceProvider = New MD5CryptoServiceProvider
+            Dim f As FileStream = New FileStream(txtSource.Text, FileMode.Open, FileAccess.Read, FileShare.Read, 8192)
+            f = New FileStream(txtSource.Text, FileMode.Open, FileAccess.Read, FileShare.Read, 8192)
+            md5.ComputeHash(f)
+            Dim hash As Byte() = md5.Hash
+            Dim buff As StringBuilder = New StringBuilder
+            Dim hashByte As Byte
+            For Each hashByte In hash
+                buff.Append(String.Format("{0:X2}", hashByte))
+            Next
+
+            If buff.ToString() = "CE218BC7088681FAA06633E218596CA7" Then
+                x = MsgBox("File hash: " & vbCrLf & buff.ToString() & vbCrLf & vbCrLf & "Windows XP Service Pack 2 kernel. Patch now?", vbOKCancel + MsgBoxStyle.Information, "BSoD-Patcher")
+                If x = MsgBoxResult.Cancel Then
+                    MsgBox("Patch aborted!", vbOKOnly + MsgBoxStyle.Information, "BSoD-Patcher")
+                    Exit Sub
+                End If
+                Try
+                    File.Copy(txtSource.Text, txtDest.Text, True)
+                    Dim encText As New System.Text.UTF8Encoding()
+                    Dim btText() As Byte
+                    Dim bw As New IO.BinaryWriter(IO.File.Open(txtDest.Text, IO.FileMode.Open, IO.FileAccess.ReadWrite))
+                    bw.BaseStream.Seek(&H1F8B1C, IO.SeekOrigin.Begin)
+                    btText = encText.GetBytes(Strings.Left(txtBlock1.Text.PadRight(96), 96))
+                    bw.Write(btText)
+                    bw.BaseStream.Seek(&H1F8BC4, IO.SeekOrigin.Begin)
+                    btText = encText.GetBytes(Strings.Left(txtBlock2.Text.PadRight(139), 139))
+                    bw.Write(btText)
+                    bw.BaseStream.Seek(&H1F8C58, IO.SeekOrigin.Begin)
+                    btText = encText.GetBytes(Strings.Left(txtBlock3.Text.PadRight(491), 491))
+                    bw.Write(btText)
+                    bw.BaseStream.Seek(&H1F8E4C, IO.SeekOrigin.Begin)
+                    btText = encText.GetBytes(Strings.Left(txtBlock5.Text.PadRight(23), 23))
+                    bw.Write(btText)
+                    bw.BaseStream.Seek(&H1F8F00, IO.SeekOrigin.Begin)
+                    btText = encText.GetBytes(Strings.Left(txtBlock7.Text.PadRight(34), 34))
+                    bw.Write(btText)
+                    bw.BaseStream.Seek(&H1F8F50, IO.SeekOrigin.Begin)
+                    btText = encText.GetBytes(Strings.Left(txtBlock9.Text.PadRight(126), 126))
+                    bw.Write(btText)
+                    If chkStop.CheckState = CheckState.Unchecked Then
+                        bw.BaseStream.Seek(&H5C92E, IO.SeekOrigin.Begin)
+                        btText = encText.GetBytes("                                          ")
+                        bw.Write(btText)
+                    End If
+                    bw.Close()
+                    strPad = vbNullString
+                    MsgBox("Patched!", vbOKOnly + MsgBoxStyle.Information, "BSoD-Patcher")
+                Catch ex As Exception
+                    MsgBox("Patch failed!", vbOKOnly + MsgBoxStyle.Exclamation, "BSoD-Patcher")
+                End Try
+            Else
+                MsgBox("File hash: " & vbCrLf & buff.ToString() & vbCrLf & vbCrLf & "Unrecognised kernel! This file cannot be patched.", vbOKOnly + MsgBoxStyle.Exclamation, "BSoD-Patcher")
+                'MsgBox("File hash: " & vbCrLf & buff.ToString() & vbCrLf & vbCrLf & "Unrecognised kernel! It is not recommended that you patch this file." & vbCrLf & "You may patch at your own risk. Patch now?", MsgBoxStyle.OkCancel + MsgBoxStyle.Exclamation, "BSoD-Patcher")
+            End If
+
+        Catch ex As Exception
+            MsgBox("File failed hash check!", vbOKOnly + MsgBoxStyle.Exclamation, "BSoD-Patcher")
+        End Try
+
+    End Sub
+
+
+
+
+
+    Private Sub cmdSourceBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSourceBrowse.Click
+        Dim fileOpen As New OpenFileDialog
+        fileOpen.InitialDirectory = Application.ExecutablePath
+        fileOpen.Filter = "Executable Files (*.exe)|*.exe"
+        fileOpen.AddExtension = True
+        fileOpen.DefaultExt = ".exe"
+        If fileOpen.ShowDialog() = Windows.Forms.DialogResult.OK Then txtSource.Text = fileOpen.FileName
+    End Sub
+
+    Private Sub cmdPatchedBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdPatchedBrowse.Click
+        Dim fileSave As New SaveFileDialog
+        fileSave.InitialDirectory = Application.ExecutablePath
+        fileSave.Filter = "Executable Files (*.exe)|*.exe"
+        fileSave.AddExtension = True
+        fileSave.DefaultExt = ".exe"
+        If fileSave.ShowDialog() = Windows.Forms.DialogResult.OK Then txtDest.Text = fileSave.FileName
+    End Sub
+
+ 
+    Private Sub cmdAbout_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdAbout.Click
+        MsgBox("BSoD-Patcher by Moonlit, v1.2.2 / March 2011" & vbCrLf & vbCrLf & "Patches a Windows kernel to create custom BSoD messages." & vbCrLf & "(Windows XP Service Pack 2 only.)" & vbCrLf & vbCrLf & "Neither the creator or distributors of this application are responsible for its (mis)use, and no guarantee is made of its fitness for any purpose." & vbCrLf & "Use at your own risk, and always make backups.", MsgBoxStyle.Question + MsgBoxStyle.OkOnly, "About...")
+    End Sub
+End Class
+
